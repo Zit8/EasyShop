5199b2b3ef66911c33c46c0c31cdaff99c8fc486
Zit <v.kaledinceff@gmail.com>
2023-03-10 11:21:43 +0300

    client ready to start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,20 @@
+import axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavBar from './components/UI/AppNavBar';
 
-function App() {
+const someNumber = (): number => Math.random();
+
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppNavBar />
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </Container>
   );
 }
 
---
+++ client/src/components/UI/OneProductCard.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function OneProductCard(): JSX.Element {
+  return <div>OneProductCard</div>;
+}
