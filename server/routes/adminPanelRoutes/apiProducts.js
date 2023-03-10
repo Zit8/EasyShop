43600c759fe8a6d609a99325b2f1e964384d2cbb
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2023-03-10 14:43:30 +0300

    create custom methods for sequelize-model Product and added endpoints for apiProducts

--- server/routes/adminPanelRoutes/apiProducts.js
+++ server/routes/adminPanelRoutes/apiProducts.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { Product } = require('../../db/models')
+const { Product } = require("../../db/models");
 
 const productRouter = express.Router();
 
 productRouter
   .route("/")
   .get(async (req, res) => {
+    // getList
     try {
       const allProducts = await Product.findAll({
         order: [["createdAt", "DESC"]],
@@ -17,6 +18,7 @@ productRouter
     }
   })
   .post(async (req, res) => {
+    // create
     console.log(req.body);
     try {
       const newPost = await Product.create({ ...req.body });
@@ -27,14 +29,58 @@ productRouter
     }
   });
 
-productRouter.route("/:id").delete(async (req, res) => {
-  try {
-    await Product.destroy({ where: { id: req.params.id } });
-    res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
+productRouter
+  .route("/many")
+  .patch(async (req, res) => {
+    // updateMany
+    try {
+      const result = await Product.updateMany(req.body);
+      console.log(result);
+      res.json({ data: result });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  })
+  .delete(async (req, res) => {
+    res.json({ data: await Product.deleteMany(req.body) });
+  });
+
+productRouter
+  .route("/:id")
+  .get(async (req, res) => {
+    // getOne
+    try {
+      const product = await Product.findByPk(req.params.id);
+      res.json({ data: product });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  })
+  .delete(async (req, res) => {
+    // delete
+    try {
+      await Product.destroy({ where: { id: req.params.id } });
+      res.json({ data: req.body });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  })
+  .patch(async (req, res) => {
+    // update
+    try {
+      await Product.update(
+        { ...(await Product.findByPk(req.params.id)), ...req.body },
+        { where: { id: req.params.id } }
+      );
+      const product = await Product.findByPk(req.params.id);
+      res.json({ data: product });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+    }
+  });
 
 module.exports = productRouter;
