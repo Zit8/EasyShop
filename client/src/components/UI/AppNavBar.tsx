d89a15584ee3fde94bdfca143e9cfae1b37b04de
Andrey Vorozhtsov <pilottu154b2@gmail.com>
2023-03-10 19:28:42 +0300

    navbar

--- client/src/components/UI/AppNavBar.tsx
+++ client/src/components/UI/AppNavBar.tsx
@@ -1,5 +1,149 @@
-import React from 'react';
+import {
+  AppBar,
+  Box,
+  Grid,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+  Container,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
+import React, { useState } from 'react';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function AppNavBar(): JSX.Element {
-  return <div>AppNavBar</div>;
+const styles = {
+  appBar: {
+    background: 'white',
+    boxShadow: 'none',
+    textShadow: '1px 1px 2px black',
+    marginBottom:'30px',
+  },
+  toolbar: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  link: {
+    color: 'black',
+    textDecoration: 'none',
+    fontFamily: 'Inter',
+    fontSize: '20px',
+    margin: '0 10px',
+  },
+  logo: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  logoImg: {
+    height: '50px',
+  },
+  shopName: {
+    fontFamily: 'Inter',
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginLeft: '10px',
+  },
+  drawer: {
+    width: '240px',
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: '240px',
+  },
+  menuIcon: {
+    color: 'black',
+    fontSize: '30px',
+  },
+  list: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '50px',
+  },
+  listItem: {
+    textAlign: 'center',
+    padding: '20px',
+  },
+};
+
+function AppNavbar(): JSX.Element {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setOpenDrawer(!openDrawer);
+  };
+
+  return (
+    <AppBar position="static" style={styles.appBar}>
+      <Container maxWidth="xl">
+        <Toolbar sx={styles.toolbar}>
+          <Grid container justifyContent="space-between" alignItems="center">
+            <Grid item>
+              <Box>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={handleDrawerToggle}
+                  sx={styles.menuIcon}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </Box>
+              <Drawer
+                variant="temporary"
+                anchor="left"
+                open={openDrawer}
+                onClose={handleDrawerToggle}
+                sx={styles.drawer}
+                classes={{ paper: styles.drawerPaper }}
+              >
+                <List sx={styles.list}>
+                  <ListItem button style={{textAlign: 'center',
+    padding: '20px',}}>
+                    <ListItemText primary="О КОМПАНИИ" />
+                  </ListItem>
+                  <ListItem button style={{textAlign: 'center',
+    padding: '20px',}}>
+                    <ListItemText primary="КОНТАКТЫ" />
+                  </ListItem>
+                </List>
+              </Drawer>
+            </Grid>
+            <Grid item>
+              <Box style={styles.logo}>
+                <Typography variant="h6" color="inherit">
+                  <img
+                    src="https://img.icons8.com/cotton/64/null/lemon-cake.png"
+                    alt="Logo"
+                    style={styles.logoImg}
+                  />
+                </Typography>
+                <Typography style={styles.shopName} variant="h4">
+                  SHOP NAME
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Box style={{ display: 'flex' }}>
+                <Link href="/" style={styles.link}>
+                  SING UP
+                </Link>
+                <Link href="/" style={styles.link}>
+                  LOGOOUT
+                </Link>
+                <ShoppingCartIcon sx={{ color: 'black', fontSize: 30 }} />
+              </Box>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
+
+export default AppNavbar;
--- client/src/components/UI/OneProductCard.tsx
+++ client/src/components/UI/OneProductCard.tsx
@@ -1,5 +1,74 @@
 import React from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
+  Grid,
+  Typography,
+  ListItem,
+  IconButton
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Rating from '@mui/material/Rating';
+import { Add, Remove } from '@mui/icons-material';
+import { Drink } from '../../types';
 
-export default function OneProductCard(): JSX.Element {
-  return <div>OneProductCard</div>;
+type OneCardProps = {
+  drink: Drink
 }
+
+export default function OneCard({drink}: OneCardProps):JSX.Element {
+  const handleOrder = () => {
+    // Handle order button click
+  };
+
+  return (
+    <Card sx={{ 
+      maxWidth: 235,
+      height: 'auto',
+      margin: 2,
+      backgroundColor: '#FAFAFA',
+      boxShadow: '0px 0px 4px 1px rgba(0,0,0,0.5)',
+      borderRadius: '8px',
+      transition: 'box-shadow 0.3s ease-in-out',
+      '&:hover': {
+        boxShadow: '0px 0px 8px 2px rgba(0,0,0,0.5)',
+      }
+    }}>
+        <CardMedia
+        sx={{marginTop:"10px", borderRadius:2}}
+          component="img"
+          height="140"
+          image={drink.strDrinkThumb}
+          alt={drink.strDrink}
+          
+        />
+        <CardContent>
+  <Grid container spacing={3} justifyContent="space-between" alignItems="center">
+    <Grid item>
+    <IconButton size="small" style={{ borderRadius: '50%', border: '1px solid black' }}>
+  <Remove />
+</IconButton>
+    </Grid>
+    <Grid item>
+    <Typography align="center" sx={{wordWrap: "break-word"}}>{drink.strDrink}</Typography>
+    </Grid>
+    <Grid item>
+    <IconButton size="small" style={{ borderRadius: '50%', border: '1px solid black' }}>
+  <Add />
+</IconButton>
+    </Grid>
+  </Grid>
+</CardContent>
+        <CardActions>
+          <Button variant="contained" fullWidth onClick={handleOrder}>
+            Заказать
+          </Button>
+        </CardActions>
+      </Card>
+  );
+};
