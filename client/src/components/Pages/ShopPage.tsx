ba051607d85e596368690d50ddcdd5bf8a6a8630
Zit <v.kaledinceff@gmail.com>
2023-03-12 16:55:55 +0300

    merge commit

--- client/src/components/Pages/ShopPage.tsx
+++ client/src/components/Pages/ShopPage.tsx
@@ -3,6 +3,8 @@ import { useAppDispatch, useAppSelector } from '../../features/reduxHooks';
 import Container from 'react-bootstrap/Container';
 import { useParams } from 'react-router-dom';
 import { getShopThunk } from '../../features/Slices/shopSlice';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function ShopPage(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -17,8 +19,41 @@ export default function ShopPage(): JSX.Element {
   }, []);
 
   return (
-    <Container className="d-flex">
-      <h1>{shop.shop.name}</h1>
+    <Container>
+      <Row className="mt-5">
+        <Col className="mt-3">1 of 2</Col>
+      </Row>
+      <Row style={{ justifyContent: 'space-between' }}>
+        <Col md="auto" style={{ width: '20%' }}>
+          <Col style={{ fontWeight: 'bold' }}>Все категории</Col>
+          <Col style={{ marginTop: '10px' }}>Шоколад ручной работы</Col>
+          <Col>Макарунс</Col>
+          <Col>Эклеры</Col>
+          <Col>Профитроли</Col>
+          <Col>Кексы</Col>
+          <Col>Чизкейк</Col>
+          <Col>Бенто торты</Col>
+        </Col>
+        <Col xs lg="2" style={{ width: '80%' }}>
+          2 of 3
+        </Col>
+      </Row>
     </Container>
+
+    // <Container style={{ display: 'flex' }}>
+    //   <Row>
+    //     <Container>
+    //       <h1>{shop.shop.name}</h1>
+    //     </Container>
+    //   </Row>
+    //   <Row>
+    //     <Col>
+    //       <Container></Container>
+    //     </Col>
+    //     <Col>
+    //       <Container></Container>
+    //     </Col>
+    //   </Row>
+    // </Container>
   );
 }
